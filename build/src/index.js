49621bcbad6a54a95257a9c2de5189d8e21624a9
Azevedo <azevedorenan13@gmail.com>
2023-09-13 16:03:13 -0300

    Adicionar funções, classes e subclasses em typescript, adicionar decorators e funções genericas

--- build/src/index.js
+++ build/src/index.js
@@ -1,5 +1,20 @@
 "use strict";
 //Variaveis
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 //tipos primitivos: boolean, number, string
 let ligado = false; //colocar ":" na variavel interpreta como uma "trava" para o parametro
 let nome = "Renan";
@@ -45,3 +60,68 @@ dados.pop();
 //Datas
 let aniversario = new Date("2022-12-01 05:00");
 console.log(aniversario.toString());
+//funções 
+function addToHello(name) {
+    return `Hello ${name}`;
+}
+function callToPhone(phone) {
+    return phone;
+}
+function addNumber(x, y) {
+    return x + y;
+}
+let soma = addNumber(4, 7);
+//Função assincrona
+function getDatabase(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return "Azevedo";
+    });
+}
+const bot = {
+    id: 1,
+    name: "Robo"
+};
+//classes
+class Personagem {
+    constructor(name, strenght, skill) {
+        //o "this" faz referencia a classe Personagem, logo atribuindo o name da classe para name
+        this.strength = strenght;
+        this.skill = skill;
+    }
+    attack() {
+        console.log(`Attack with ${this.strength} points`);
+    }
+}
+const p1 = new Personagem("Azevedo", 10, 98);
+p1.attack();
+//data modifiers
+//Public, private e protected
+//subclasses
+//Personagem é a superclasse
+//Magician é uma subclasse
+class Magician extends Personagem {
+    constructor(name, strength, skill) {
+        super(name, strength, skill); //passar os parametros da superclasse
+    }
+}
+//generics
+function concatArray(...itens) {
+    return new Array().concat(...itens);
+}
+const stgArray = concatArray(["Azevedo, Oliveira"], ["Santos"]); //Tipo string
+const numArray = concatArray([1, 5], [3]); //Tipo number
+console.log(numArray);
+//decorators
+function exibirNome(target) {
+    console.log(target);
+}
+let Funcionario = class Funcionario {
+};
+Funcionario = __decorate([
+    exibirNome
+], Funcionario);
+let Quincas = class Quincas {
+};
+Quincas = __decorate([
+    exibirNome
+], Quincas);
--- src/index.ts
+++ src/index.ts
@@ -79,7 +79,7 @@ function addToHello(name: string){
     return `Hello ${name}`;
 }
 
-function callToPhone(phone: number | string){  //numero ou string
+function callToPhone(phone: number | string): number | string{  //numero ou string
     return phone;
 }
 
@@ -88,5 +88,130 @@ function addNumber(x: number, y: number): number{  //recebe dois parametros tipo
 }
 let soma:number = addNumber(4,7);
 
+ //Função assincrona
+async function getDatabase(id:number): Promise<string> {
+    return "Azevedo";
+}
 
- 
+//Interfaces
+
+type robot = {    //type: utilizado para tipar
+    id:number,    //interface: utilizado para tipar variaveis especificas, interpretar como se fosse um contrato
+    name:string,
+};
+
+interface robot2{
+    id:number,
+    name:string,
+}
+
+const bot: robot = {
+    id: 1,
+    name: "Robo"
+}
+
+//classes
+class Personagem{
+    name?: string;
+    strength: number;
+    skill: number;
+
+    constructor(name:string, strenght:number, skill:number){
+         //o "this" faz referencia a classe Personagem, logo atribuindo o name da classe para name
+        this.strength = strenght;
+        this.skill = skill;
+    }
+
+    attack(): void{
+        console.log(`Attack with ${this.strength} points`)
+    }
+}
+
+
+    const p1 = new Personagem("Azevedo",10, 98);
+    p1.attack();
+
+//data modifiers
+//Public, private e protected
+
+//subclasses
+
+//Personagem é a superclasse
+//Magician é uma subclasse
+class Magician extends Personagem{
+    constructor(name: string, strength:number, skill:number){
+    super(name,strength,skill) //passar os parametros da superclasse
+    }
+}
+
+//generics
+function concatArray<T>(...itens: T[]):T[]{ // "T" para definir o tipo do parametro (linha 152 e 153)
+    return new Array().concat(...itens);
+}
+
+const stgArray = concatArray<string[]>(["Azevedo, Oliveira"],["Santos"])   //Tipo string
+const numArray = concatArray<number[]>([1, 5], [3]);                       //Tipo number
+console.log(numArray);
+
+//decorators
+
+function exibirNome(target: any){
+    console.log(target);
+}
+
+@exibirNome
+class Funcionario {
+
+}
+
+@exibirNome
+class Quincas {}
+
+
+function apiVersion(version:string){
+    return(target: any) => {
+        Object.assign(target.prototype, {__version: version});
+    };
+}
+
+function minLenght(lenght:number){
+    return(target:any, key: string) => {
+        let _value = target[key];
+
+        const getter = () => _value;
+        const setter = (value: string) => {
+            if (value.length< lenght) {
+                throw new Error(`Tamanho menor do que ${length}`);
+            }else{
+                _value = value
+            }
+        };
+
+        Object.defineProperty(target, key,{
+        get: getter,
+        set: setter,
+         });
+    };
+}
+
+@apiVersion("1.10")
+class Api{}
+
+const api = new Api();
+//console.log(api); //api.__version
+
+//attribute decorator
+
+class Api2 {
+    @minLenght(3)
+    name:string;
+    
+    constructor(name:string){
+        this.name = name;
+    };
+}
+
+const api2 = new Api2("produtos");
+console.log(api2.name);
+
+
